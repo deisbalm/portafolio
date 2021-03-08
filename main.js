f5de0907df947e9900b487f8628308cfab111ed2
DENISE BALMACEDA <deisbalm@gmail.com>
2021-03-07 21:06:50 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('#sobre-mi');
+ScrollReveal().reveal('#habilidades',{delay:600});
+ScrollReveal().reveal('#educacion',{delay:600});
+ScrollReveal().reveal('#contacto',{delay:600});
